523075cbb3bc948de6b91e19cf2a6fb445d05d49
ken <itisthecon@gmail.com>
2016-12-31 11:29:42 +0800

    按ruby习惯方式重写

--- yd.rb
+++ yd.rb
@@ -1,21 +1,22 @@
-#!/usr/bin/env ruby
+#!/usr/bin/ruby
 require 'json'
 require 'open-uri'
 require 'mechanize'
 require 'colorize'
 
 if ARGV.empty?
-	abort ("有道在线词典控制台Ruby版\n用法:yd 单词 单词 ...")
+    abort ("有道在线词典控制台Ruby版\n用法:yd 单词 单词 ...")
 end
 error_code = {
-	0 => '正常',
-	20 => '要翻译的文本过长',
-	30 => '无法进行有效的翻译',
-	40 => '不支持的语言类型',
-	50 => '无效的key',
-	60 => '无词典结果，仅在获取词典结果生效'
+    0 => '正常',
+    20 => '要翻译的文本过长',
+    30 => '无法进行有效的翻译',
+    40 => '不支持的语言类型',
+    50 => '无效的key',
+    60 => '无词典结果，仅在获取词典结果生效'
 }
 agent = Mechanize.new
+agent.robots = false
 agent.user_agent_alias = 'Mac Safari'
 query = URI::encode(ARGV.join(' '))
 url = 'http://fanyi.youdao.com/openapi.do?keyfrom=google-youdao&key=814771929&type=data&doctype=json&version=1.1&q='
@@ -23,51 +24,40 @@ url += query
 begin
     json = agent.get(url).body
 rescue Exception => ex
-    log.error "Error: #{ex}"
+    puts "Error: #{ex.message}"
 end
 result = JSON.parse(json)
 errorcode = result['errorCode']
 if errorcode != 0
-	abort (error_code[errorcode])
+    abort (error_code[errorcode])
 end
 query = result['query'] + ':'
-has_phonetic = false
-has_basic = false
-if result['basic']
-    has_basic =true
-end
-if has_basic
-    if result['basic']['phonetic']
-        has_phonetic = true
+phonetic = ''
+unless result['basic'].empty?
+    unless result['basic']['phonetic'].empty?
         phonetic = '[' + result['basic']['phonetic'] + ']'
         phonetic = '音标: '.colorize(:color => :cyan,:mode => :bold) + phonetic.colorize(:yellow)
-        if result['basic']['uk-phonetic']
+        unless result['basic']['uk-phonetic'].empty?
             uk_phonetic = '[' + result['basic']['uk-phonetic'] + ']'
             phonetic += "\t" + '英音: '.colorize(:color => :cyan,:mode => :bold) + uk_phonetic.colorize(:yellow)
         end
-        if result['basic']['us-phonetic']
+        unless result['basic']['us-phonetic'].empty?
             us_phonetic = '[' + result['basic']['us-phonetic'] + ']'
             phonetic += "\t" + '美音: '.colorize(:color => :cyan,:mode => :bold) + us_phonetic.colorize(:yellow)
         end
     end
 end
 puts query.colorize(:color => :light_white,:mode => :bold)
-if has_phonetic
+unless phonetic.empty?
     puts phonetic
 end
 puts '翻译:'.colorize(:color => :cyan,:mode => :bold)
-for trans in result['translation']
-    puts "\t" + trans.colorize(:yellow)
-end
-if has_basic
+result['translation'].each {|trans| puts "\t" + trans.colorize(:yellow)}
+unless result['basic'].empty?
     puts '基本词典:'.colorize(:color => :cyan,:mode => :bold)
-    for basedic in result['basic']['explains']
-        puts "\t" + basedic.colorize(:yellow)
-    end
+    result['basic']['explains'].each {|basedic| puts "\t" + basedic.colorize(:yellow)}
 end
-if result['web']
+unless result['web'].empty?
     puts '网络释义:'.colorize(:color => :cyan,:mode => :bold)
-    for web in result['web']
-        puts "\t" + web['key'].colorize(:blue) + ' : ' + web['value'].join(',')
-    end
+    result['web'].each {|web| puts "\t" + web['key'].colorize(:blue) + ' : ' + web['value'].join(',')}
 end
