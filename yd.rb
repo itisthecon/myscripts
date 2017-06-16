2e59897ce0cf741cb020300c8dae62be6895ab14
itisthecon <itisthecon@gmail.com>
2017-06-16 14:38:05 +0800

    yd更新

--- yd.rb
+++ yd.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 require 'json'
 require 'open-uri'
 require 'mechanize'
@@ -16,7 +16,6 @@ error_code = {
 	60 => '无词典结果，仅在获取词典结果生效'
 }
 agent = Mechanize.new
-agent.robots = false
 agent.user_agent_alias = 'Mac Safari'
 query = URI::encode(ARGV.join(' '))
 url = 'http://fanyi.youdao.com/openapi.do?keyfrom=google-youdao&key=814771929&type=data&doctype=json&version=1.1&q='
@@ -24,7 +23,7 @@ url += query
 begin
     json = agent.get(url).body
 rescue Exception => ex
-    puts "Error: #{ex.message}"
+    log.error "Error: #{ex}"
 end
 result = JSON.parse(json)
 errorcode = result['errorCode']
@@ -32,32 +31,43 @@ if errorcode != 0
 	abort (error_code[errorcode])
 end
 query = result['query'] + ':'
-phonetic = ''
-unless result['basic'].nil?
-    unless result['basic']['phonetic'].nil?
+has_phonetic = false
+has_basic = false
+if result['basic']
+    has_basic =true
+end
+if has_basic
+    if result['basic']['phonetic']
+        has_phonetic = true
         phonetic = '[' + result['basic']['phonetic'] + ']'
         phonetic = '音标: '.colorize(:color => :cyan,:mode => :bold) + phonetic.colorize(:yellow)
-        unless result['basic']['uk-phonetic'].empty?
+        if result['basic']['uk-phonetic']
             uk_phonetic = '[' + result['basic']['uk-phonetic'] + ']'
             phonetic += "\t" + '英音: '.colorize(:color => :cyan,:mode => :bold) + uk_phonetic.colorize(:yellow)
         end
-        unless result['basic']['us-phonetic'].empty?
+        if result['basic']['us-phonetic']
             us_phonetic = '[' + result['basic']['us-phonetic'] + ']'
             phonetic += "\t" + '美音: '.colorize(:color => :cyan,:mode => :bold) + us_phonetic.colorize(:yellow)
         end
     end
 end
 puts query.colorize(:color => :light_white,:mode => :bold)
-unless phonetic.empty?
+if has_phonetic
     puts phonetic
 end
 puts '翻译:'.colorize(:color => :cyan,:mode => :bold)
-result['translation'].each {|trans| puts "\t" + trans.colorize(:yellow)}
-unless result['basic'].nil?
-    puts '基本词典:'.colorize(:color => :cyan,:mode => :bold)
-    result['basic']['explains'].each {|basedic| puts "\t" + basedic.colorize(:yellow)}
+for trans in result['translation']
+    puts "\t" + trans.colorize(:yellow)
 end
-unless result['web'].nil?
+if has_basic
+    puts '基本词典:'.colorize(:color => :cyan,:mode => :bold)
+    for basedic in result['basic']['explains']
+        puts "\t" + basedic.colorize(:yellow)
+    end
+end
+if result['web']
     puts '网络释义:'.colorize(:color => :cyan,:mode => :bold)
-    result['web'].each {|web| puts "\t" + web['key'].colorize(:blue) + ' : ' + web['value'].join(',')}
+    for web in result['web']
+        puts "\t" + web['key'].colorize(:blue) + ' : ' + web['value'].join(',')
+    end
 end
