415312d36a40fdc0785211ccca8795ddd8b03a2b
itisthecon <itisthecon@gmail.com>
2020-01-14 16:13:53 +0800

    ruby version eptime optimize

--- eptime.rb
+++ eptime.rb
@@ -6,8 +6,13 @@ require 'date'
 params = ARGV.getopts("d:")
 
 unless params["d"].nil?
-  puts DateTime.parse("#{params["d"]} +0800").to_datetime.to_time.to_i
+  puts DateTime.parse("#{params["d"]} #{Time.now.strftime("%z")}").to_datetime.to_time.to_i
   puts params["d"]
+end
+
+unless ARGV[0].empty?
+  puts ARGV[0]
+  puts Time.at(ARGV[0].to_i).to_s
 end
 
 puts Time.now.to_i
