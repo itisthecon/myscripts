b1774518e18729f24d8ad815ec28ecebadd8013b
itisthecon <itisthecon@gmail.com>
2020-04-21 11:18:29 +0800

    ruby difftree optimize

--- difftree.rb
+++ difftree.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'find'
 require 'digest'
@@ -6,14 +7,14 @@ require 'pathname'
 require 'colorize'
 
 if ARGV.length != 2
-  puts "useage: difftree dir1 dir2"
+  puts 'useage: difftree dir1 dir2'
   exit
 end
 
 pwd = Dir.pwd + '/'
 (dir1, dir2) = ARGV.map do |arg|
   arg = pwd + arg if arg[0] != '/'
-  arg = arg + '/' if arg[-1] != '/'
+  arg += '/' if arg[-1] != '/'
   arg
 end
 
@@ -24,9 +25,11 @@ Find.find(dir1) do |path|
   path2 = dir2 + rel_path.to_s
 
   unless File.exist?(path2)
-    puts "!!! ".colorize(:yellow).blink + path2.colorize(:red) + " do not exist"
+    puts '!!! '.colorize(:yellow).blink + path2.colorize(:red) + ' do not exist'
     next
   end
 
-  puts `/usr/local/bin/colordiff -u #{path} #{path2}` unless Digest::MD5.file(path).hexdigest == Digest::MD5.file(path2).hexdigest
+  unless Digest::MD5.file(path).hexdigest == Digest::MD5.file(path2).hexdigest
+    puts `/usr/local/bin/colordiff -u #{path} #{path2}`
+  end
 end
